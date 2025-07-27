6b60b0330ec9f03d80ad73aa47bbcd5dcb4a6bf9
Sanawar <abrarkhanmohammadsanawar@gmail.com>
2025-07-27 18:31:17 +0500

    Added histogram,   and boxplot visualizations for player data analysis

--- fifa_21.r
+++ fifa_21.r
@@ -4,7 +4,7 @@ library(ggplot2) # Used for data visualization
 library(readr) # Used for reading data
 library(GGally) # Used to visualize relationship between data
 library(skimr) # Used for power analysis
-library(corrplot) # Used to view co-relation between the variables
+library(corrplot) # Used to view correlation between the variables
 library(reshape2) # USed to deal with dataframes
 
 # Loading the dataset
@@ -26,4 +26,38 @@ fifa21df = fifa21 %>%
         "defending",
         "physic"
     )
+
 view(fifa21df)
+
+# Gives information about variable types and number of missing values etc
+view(skimr(fifa21df))
+
+# Plotting Histograms
+ggplot(fifa21df, mapping = aes(x = age)) +
+    geom_histogram(bins = 60, fill = "steelblue", col = "white") +
+    labs(title = "Player's Age Histogram")
+
+ggplot(fifa21df, mapping = aes(x = height_cm)) +
+    geom_histogram(bins = 60, fill = "steelblue", col = "white") +
+    labs(title = "Player's Age Histogram")
+
+ggplot(fifa21df, mapping = aes(x = weight_kg)) +
+    geom_histogram(bins = 60, fill = "steelblue", col = "white") +
+    labs(title = "Player's Age Histogram")
+
+# Correlation between variables
+cor.matrix = cor(na.omit(fifa21df[, -c(1, 2)]))
+corrplot(cor.matrix, type = )
+
+# Box-plot of variables
+ggplot(data  = melt(fifa21df), mapping = aes(x = variable, y = value)) +
+     geom_boxplot()
+
+# Outlier Detection
+out <- boxplot.stats(fifa21df$age)$out
+out_ind <- which(fifa21df$age%in% c(out))
+out_ind
+fifa21df$weight_kg[c(13,103)]
+fifa21df$short_name[c(13,103)]
+
+
